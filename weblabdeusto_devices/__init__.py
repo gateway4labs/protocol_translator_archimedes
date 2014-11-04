2fb87b9d7916c58d8ac83e2de0943108c529f135
Pablo Orduña <pablo.orduna@deusto.es>
2014-11-04 14:04:51 +0100

    Add initial testing tool

--- weblabdeusto_devices/__init__.py
+++ weblabdeusto_devices/__init__.py
@@ -2,7 +2,8 @@ import urlparse
 
 import redis
 import time
-from flask import Flask, request
+import json
+from flask import Flask, request, render_template, render_template_string
 from flask_sockets import Sockets
 
 app = Flask(__name__)
@@ -16,31 +17,43 @@ def echo_socket(ws):
     arguments = urlparse.parse_qs(ws.environ['QUERY_STRING'])
     # { 'reservation_id' : ['foo'] }
     reservation_ids = arguments.get('reservation_id', [])
-    sensor_ids = arguments.get('sensor_id', [])
     lab_ids = arguments.get('lab_id', [])
 
-    if reservation_ids and sensor_ids and lab_ids:
+    if reservation_ids and lab_ids:
         reservation_id = reservation_ids[0]
-        sensor_id = sensor_ids[0]
         lab_id = lab_ids[0]
     else:
-        ws.send("ERROR: fields missing (check reservation_id, sensor_id and lab_id)")
+        ws.send(json.dumps({ 'error' : True, 'message' : "fields missing (check reservation_id and lab_id)" }))
         ws.close()
         return
 
     if lab_id not in LABS:
-        ws.send("ERROR: invalid lab (not found in list)")
+        ws.send(json.dumps({ 'error' : True, 'message' : "invalid lab (not found in list)" }))
         ws.close()
         return
 
-    status.new_reservation(reservation_id, LABS[lab_id])
+    current_lab = LABS[lab_id]
 
-    previous_data = {}
-    for new_data in status.get_notifications(reservation_id):
-        ws.send("Sensor data for %s: %s" % (reservation_id, time.asctime()))
+    status.new_reservation(reservation_id, current_lab)
 
+    while True:
+        txtmsg = ws.receive()
+        message = json.loads(txtmsg)
+        method = message.get('method')
+        if method == 'getSensorMetadata':
+            metadata = current_lab.get_sensor_metadata(reservation_id, message)
+            ws.send(json.dumps(metadata))
+        elif method == 'getSensorData':
+            pass
+        else:
+            ws.send(json.dumps({ 'error' : True, 'message' : 'Message without method'}))
+
+#     previous_data = {}
+#     for new_data in status.get_notifications(reservation_id):
+#         ws.send(new_data)
+# 
     ws.close()
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template_string('''<html><body>Try the <a href="{{ url_for('static', filename='testing.html') }}">testing tool</a></body></html>''')
--- weblabdeusto_devices/ext/archimedes.py
+++ weblabdeusto_devices/ext/archimedes.py
@@ -1,6 +1,70 @@
 STATUS_COMMAND = 'ALLINFO:archimedes1:archimedes2:archimedes3:archimedes4:archimedes5:archimedes6:archimedes7'
 
-def process_status(old_result, new_result, sensor_id):
-    return 5
+def get_sensor_metadata(reservation_id, message):
+    metadata = {
+        "method" : "getSensorMetadata",
+        "sensors" : []
+    }
+    
+    for number in range(1, 8):
+        sensor_metadata = {
+                    "sensorId" : "tube-%s" % number,
+                    "fullName" : "Test tube %s" % number ,
+                    "description" : "The test tube where the ball #%s is" % number,
+                    "webSocketType" : "text",
+                    "produces" : "application/json",
+                    "configuration" : [],
+                    "accessMode" : { 
+                         "type" : "push",
+                         "nominalUpdateInterval" : 3000, # 3 seconds
+                         "userModifiableFrequency" : False
+                    },
+                    "values" : [
+                            {
+                                   "name" : "level",
+                                   "unit" : "cm",
+                                   # "lastMeasured" : "2014-...",
+                                   "rangeMinimum" : 0.0,
+                                   "rangeMaximum" : 100.0,
+                                   "rangeStep" : 0.1,
+                                   "updateFrequency" : 3000, # 3 seconds
+                            },
+                            {
+                                   "name" : "load",
+                                   "unit" : "gr",
+                                   # "lastMeasured" : "2014-...",
+                                   "rangeMinimum" : 0.0,
+                                   "rangeMaximum" : 100.0,
+                                   "rangeStep" : 0.1,
+                                   "updateFrequency" : 3000,
+                            }
+                    ]
+           }
+        metadata['sensors'].append(sensor_metadata)
+           
+    for number in range(1, 8):
+        cam_metadata = {
+                "sensorId" : "video-%s" % number,
+                "fullName" : "Video Test Tube %s" % number,
+                "description" : "Video stream for the test tube %s" % number,
+                "webSocketType" : "binary",
+                "singleWebSocketRecommended" : True,
+                "produces" : "image/jpeg",
+                "configuration" : [], # Nothing can be configured
+                "accessMode" : {
+                      "type" : "stream",
+                      "nominalUpdateInterval" : 500, # 0.5 seconds
+                      "userModifiableFrequency" : False
+                },
+                "values" : [
+                      {
+                           "name" : "front",
+                           # "lastMeasured" : "2014-...",
+                           "updateFrequency" : 500,
+                      }
+                ]
+        }
+        metadata['sensors'].append(cam_metadata)
 
+    return metadata
 
