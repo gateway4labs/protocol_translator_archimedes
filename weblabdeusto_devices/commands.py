0ad129e9a5898a9663b802319e58b0812106cb4c
Pablo Orduña <pablo.orduna@deusto.es>
2014-11-04 11:29:55 +0100

    Make all values random

--- weblabdeusto_devices/commands.py
+++ weblabdeusto_devices/commands.py
@@ -29,8 +29,21 @@ def update_status(fake = False):
         }
         if fake:
             import random
-            response = { 'result' : { 'commandstring' : {"archimedes1": {"load": str(random.randint(0,100)), "level": "18.1"}, "archimedes2": {"load": "41.0", "level": "18.9"}, "archimedes3": {"load": "3.04", "level": "18.4"}, "archimedes4": {"load": "31.07", "level": "18.5"}, "archimedes5": {"load": "620.01", "level": "11.4"}, "archimedes6": {"load": "89.59", "level": "18.4"}, "archimedes7": {"load": "43.59", "level": "16.7"}} }, 'is_exception' : False }
-            response = { 'result' : { 'commandstring' : {"archimedes1": {"load": "41.07", "level": "18.1"}, "archimedes2": {"load": "41.0", "level": "18.9"}, "archimedes3": {"load": "3.04", "level": "18.4"}, "archimedes4": {"load": "31.07", "level": "18.5"}, "archimedes5": {"load": "620.01", "level": "11.4"}, "archimedes6": {"load": "89.59", "level": "18.4"}, "archimedes7": {"load": "43.59", "level": "16.7"}} }, 'is_exception' : False }
+            response = {'is_exception': False,
+                         'result': {'commandstring': {'archimedes1': {'level': str(random.randint(0, 10000) / 100.0),
+                                                                      'load' : str(random.randint(0, 10000) / 100.0)},
+                                                      'archimedes2': {'level': str(random.randint(0, 10000) / 100.0),
+                                                                      'load' : str(random.randint(0, 10000) / 100.0)},
+                                                      'archimedes3': {'level': str(random.randint(0, 10000) / 100.0),
+                                                                      'load' : str(random.randint(0, 10000) / 100.0)},
+                                                      'archimedes4': {'level': str(random.randint(0, 10000) / 100.0),
+                                                                      'load' : str(random.randint(0, 10000) / 100.0)},
+                                                      'archimedes5': {'level': str(random.randint(0, 10000) / 100.0),
+                                                                      'load' : str(random.randint(0, 10000) / 100.0)},
+                                                      'archimedes6': {'level': str(random.randint(0, 10000) / 100.0),
+                                                                      'load' : str(random.randint(0, 10000) / 100.0)},
+                                                      'archimedes7': {'level': str(random.randint(0, 10000) / 100.0),
+                                                                      'load' : str(random.randint(0, 10000) / 100.0)}}}}
         else:
             response = requests.post('%sjson/' % base_url, data = request, cookies = cookies).json()
 
@@ -42,10 +55,4 @@ def update_status(fake = False):
             print reservation_id
             print result
             status.notify(result, reservation_id)
-        # Obtain from redis the current reservations
-        # one by one, do the following:
-        # a) run the required command
-        # b) get the results. If the reservation is wrong or whatever, simply remove it from the system
-        # c) sleep for the next loop (which is sleeping X seconds - current, if > 0)
-        
 
