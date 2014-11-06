17e7cdc055ec8096d9ba50abce8d6e0b9d462662
Pablo Orduña <pablo.orduna@deusto.es>
2014-11-06 19:03:05 +0100

    enable fake mode in loop.py

--- loop.py
+++ loop.py
@@ -1,11 +1,17 @@
 import os
+import sys
 import time
 
 SECONDS = 3
 
+FAKE = len(sys.argv) > 1 and sys.argv[1] == '--fake'
+
 while True:
     initial = time.time()
-    os.system("python manage.py update_status")
+    if FAKE:
+        os.system("python manage.py update_status --fake")
+    else:
+        os.system("python manage.py update_status --fake")
     end = time.time()
     total_time = end - initial
     remaining = SECONDS - total_time
